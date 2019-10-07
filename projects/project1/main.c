7c54dc4d20597866fba9c433aea816db5f45318b
Mitchell Moore <mitchell1484@gmail.com>
2019-10-06 20:55:55 -0500

    edit temp dir main

--- projects/project1/main.c
+++ projects/project1/main.c
@@ -7,9 +7,13 @@ Project #:1
 
 int main(int argv, char** argc){
 	int i;
-	for (i = 0; i < argv; i++)
-	printf("Arg %d: %s\n", i, argc[i]);
-
+	if (argv == 1) {
+		printf("Tracing route....\n");
+		//Todo: Directory list from current dir
+	}else {
+		for (i = 0; i < argv; i++)
+		printf("Arg %d: %s\n", i, argc[i]);
+	}
 	return 0;
 }
 
