df54137553ebbc82bf9308c1dd097c86c6d7aac1
Mitchell Moore <mitchell1484@gmail.com>
2019-10-07 09:38:15 -0500

    Add basic traversal function

--- projects/project1/main.c
+++ projects/project1/main.c
@@ -1,19 +1,49 @@
-#include<stdio.h>
 /*
 Name: Mitchell Moore
 BlazerId:mmoo97
 Project #:1
 */
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+
+void listdir(const char *name, int indent)
+{
+    DIR *dir;
+    struct dirent *entry;
+
+    if (!(dir = opendir(name)))
+	printf("Directory does not exist.\n");
+        return;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            char path[1024];
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                continue;
+            snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
+            printf("%*s[%s]\n", indent, "", entry->d_name);
+            listdir(path, indent + 2);
+        } else {
+            printf("%*s- %s\n", indent, "", entry->d_name);
+        }
+    }
+    closedir(dir);
+}
+
 int main(int argv, char** argc){
 	int i;
-	if (argv == 1) {
+/*	if (argv == 1) {
 		printf("Tracing route....\n");
 		//Todo: Directory list from current dir
 	}else {
 		for (i = 0; i < argv; i++)
 		printf("Arg %d: %s\n", i, argc[i]);
-	}
+	}*/
+	listdir("main.c", 0);
 	return 0;
 }
 
