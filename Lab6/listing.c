7e9735a7834923aae8baaefd0046250815036217
Mitchell Moore <mitchell1484@gmail.com>
2019-10-10 20:53:01 -0500

    Lab6-Completed

--- Lab6/listing.c
+++ Lab6/listing.c
@@ -34,7 +34,7 @@ struct listing getfields(char* line){
 	return item;
 }
 
-void displayStruct(struct listing item) {
+void displayStruct(struct listing item) { 
 	printf("ID : %d\n", item.id);
 	printf("Host ID : %d\n", item.host_id);
 	printf("Host Name : %s\n", item.host_name);
@@ -48,6 +48,55 @@ void displayStruct(struct listing item) {
 	printf("Number of Reviews : %d\n", item.number_of_reviews);
 	printf("Calculated Host Listings Count : %d\n", item.calculated_host_listings_count);
 	printf("Availability_365 : %d\n\n", item.availability_365);
+}
+
+void swap(struct listing *xp, struct listing *yp) { 
+    struct listing temp = *xp;
+    *xp = *yp; 
+    *yp = temp; 
+} 
+
+void sort(struct listing arr[22555], int n, int field) { // field = 1 means sort by host name, field = 2 is by price
+	if (field == 2) {
+		int i, j; 
+		for (i = 0; i < n-1; i++)       
+			for (j = 0; j < n-i-1; j++)  
+				if (arr[j].price > arr[j+1].price) 
+				  swap(&arr[j], &arr[j+1]); 
+    }
+
+    if (field == 1) {
+		int i, j; 
+		for (i = 0; i < n-1; i++)       
+			for (j = 0; j < n-i-1; j++)  
+				if (strcmp(arr[j].host_name, arr[j+1].host_name) > 0) 
+				  swap(&arr[j], &arr[j+1]); 
+    }
+
+}
+
+void writeFile(char *dir, struct listing arr[22555]){
+	int i;
+
+	FILE *f = fopen(dir, "wb");
+	
+	for(i=0;i<22520;i++) { // change for big array
+        fprintf(f, "ID : %d\n", arr[i].id);
+		fprintf(f, "Host ID : %d\n", arr[i].host_id);
+		fprintf(f, "Host Name : %s\n", arr[i].host_name);
+		fprintf(f, "Neighbourhood Group : %s\n", arr[i].neighbourhood_group);
+		fprintf(f, "Neighbourhood : %s\n", arr[i].neighbourhood);
+		fprintf(f, "Latitude : %f\n", arr[i].latitude);
+		fprintf(f, "Longitude : %f\n", arr[i].longitude);
+		fprintf(f, "Room Type : %s\n", arr[i].room_type);
+		fprintf(f, "Price : %f\n", arr[i].price);
+		fprintf(f, "Minimum Nights : %d\n", arr[i].minimum_nights);
+		fprintf(f, "Number of Reviews : %d\n", arr[i].number_of_reviews);
+		fprintf(f, "Calculated Host Listings Count : %d\n", arr[i].calculated_host_listings_count);
+		fprintf(f, "Availability_365 : %d\n\n", arr[i].availability_365);
+     }
+
+	fclose(f);
 }
 
 int main(int argc, char* args[]) {
@@ -65,10 +114,19 @@ int main(int argc, char* args[]) {
 	while (fgets(line, LINESIZE, fptr) != NULL){
 		list_items[count++] = getfields(line);
 	}
+
 	fclose(fptr);
 	
-	for (i=0; i<count; i++)
-		displayStruct(list_items[i]);
+	// for (i=0; i<count; i++)
+	// 	displayStruct(list_items[i]); // use for output view
+
+	sort(list_items, 22520, 1);
+
+	writeFile("sorted_name.txt", list_items);
+
+	sort(list_items, 22520, 2);
+
+	writeFile("sorted_price.txt", list_items);
 	
 	return 0;
 }
